0b0206fef9fa2e06a02a02f88e00a4b2239c2936
1335877357@qq.com <1335877357@qq.com>
2020-10-14 16:16:26 +0800

    //1.关于Springboot的一系列内容

--- src/main/java/com/zrt/ad/config/MySQLConfig.java
+++ src/main/java/com/zrt/ad/config/MySQLConfig.java
@@ -1,9 +1,19 @@
 package com.zrt.ad.config;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
 /**
  * @Author:zrt
  * @Description:com.zrt.ad.config
  * @version:1.0
  */
+@Data
+@Component
+@ConfigurationProperties(prefix = "com.zrt.ad")
 public class MySQLConfig {
+     private String host;
+     private String password;
+     private  String username;
 }
